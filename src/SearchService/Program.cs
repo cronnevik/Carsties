36b73dbfd1b522358f02c3db244b0b1d179d67c8
Christian Rønnevik <cronnevik@gmail.com>
2024-02-25 11:11:27 +0100

    End of section 7

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -19,6 +19,12 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) => 
     {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
         // retry policy for specific endpoints
         cfg.ReceiveEndpoint("search-auction-created", e => 
         {
