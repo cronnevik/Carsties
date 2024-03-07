e5fae8836447edb5f8c28032dcef6b832b6f54ef
Christian Rønnevik <cronnevik@gmail.com>
2024-03-07 14:40:47 +0100

    End of section 12

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -5,18 +5,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options => 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
         // tells the resource server who the token was issued by
-        options.Authority = builder.Configuration["IdentityServiceUrl"]; 
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
         // Because the identity server is running on http:
         options.RequireHttpsMetadata = false;
         options.TokenValidationParameters.ValidateAudience = false;
         options.TokenValidationParameters.NameClaimType = "username";
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", b =>
+    {
+        b.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
